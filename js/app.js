ba35f474d068da3ed2ac48a78f8785ac0f755188
Jan Podgorski <j.pod.12@gmail.com>
2018-05-29 22:16:07 +0200

    starting parameters added

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@
 var Enemy = function(x,y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-
+    
     //coordinated parameters created
     this.x = x;
     this.y = y;
 
     //parameter for enemy speed
     this.speed = speed;
+
+      // The image/sprite for our enemies, this uses
+     // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
