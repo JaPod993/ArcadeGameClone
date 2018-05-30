a113898e246eb2a1a09180d0d33328c07196a5a2
Jan Podgorski <j.pod.12@gmail.com>
2018-05-30 20:06:55 +0200

    added title and instructions

--- js/engine.js
+++ js/engine.js
@@ -25,7 +25,7 @@ var Engine = (function(global) {
         lastTime;
 
     canvas.width = 505;
-    canvas.height = 855;
+    canvas.height = 760;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
