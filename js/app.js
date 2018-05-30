bfa4c98f3873ba6455a959b6b46611b9b142d152
Jan Podgorski <j.pod.12@gmail.com>
2018-05-30 17:51:48 +0200

    instantiated enemies and player position

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x,y, speed) {
+const Enemy = function(x,y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -42,9 +42,9 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y) {
+const Player = function(x, y) {
   this.x =x;
-  thix.y =y;
+  this.y =y;
   this.width = 50;
   this.height = 75;
   this.sprite = 'images/char-princess-girl.png';
@@ -66,13 +66,20 @@ Player.prototype.update = function(dt) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [
+    new Enemy(-200, 83, 150),
+    new Enemy(-200, 166, 230),
+    new Enemy(-200, 249, 270),
+];
+
+const player = new Player(202, 415);
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
